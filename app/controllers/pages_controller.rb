ee059cfeb250ea65274c8c212e02e8f5a88ab616
Junhyung Kim <kimjunh@berkeley.edu>
2015-02-19 17:59:21 -0800

    second commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+  def about
+  	@age = 8994
+  	@major = "Easy Engineering and Chill Studies"
+  	@song = "Jesus Loves Me This I know"
+  end
 end
